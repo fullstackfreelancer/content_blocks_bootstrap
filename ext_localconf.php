b350b51618b73a7dc7673cc8aa0714c9b6c9e406
Simon Kohler <simon@kohlercode.com>
2026-02-04 14:16:15 -0500

    Clean up, create site set

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,3 @@
 <?php
 declare(strict_types=1);
 defined('TYPO3') or die();
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-ExtensionManagementUtility::addPageTSConfig('
-    @import "EXT:content_blocks_bootstrap/Configuration/page.tsconfig"
-');
-
-ExtensionManagementUtility::addTypoScript(
-    'content_blocks_bootstrap',
-    'setup',
-    '@import "EXT:content_blocks_bootstrap/Configuration/setup.typoscript"'
-);
