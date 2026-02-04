f8d5943788ed4031475e1dc828e501f645f79f17
Simon Kohler <simon@kohlercode.com>
2026-02-04 01:42:37 -0500

    Small fixes

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,6 @@ defined('TYPO3') or die();
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-
 ExtensionManagementUtility::addPageTSConfig('
     @import "EXT:content_blocks_bootstrap/Configuration/page.tsconfig"
 ');
